e2e2068d261cd7b07d31a39d3ea822464ae70914
Youssef Gerges <youssefgerges12000@gmail.com>
2025-03-07 02:41:24 +0400

    fix build

--- api/cart/addToCart.js
+++ api/cart/addToCart.js
@@ -1,9 +1,6 @@
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {api, token} from '../api';
 import toast from 'react-hot-toast';
-import {useContextElement} from "@/context/Context";
-import bootstrap from "bootstrap";
-import {router} from "next/client";
 
 export const useAddToCart = () => {
     const queryClient = useQueryClient();
